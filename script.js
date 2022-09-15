5a789c8de0dc70094010a75049e5fb17e6f4f144
unknown <aztoorabally7447@gmail.com>
2022-09-15 16:19:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log(document.querySelector("Audio").outerHTML);
+// document.querySelector("Audio").src='./assets/Phoenix (ft. Cailin Russo and Chrissy Costanza) _ Worlds 2019 - League of Legends.mp3';
+document.querySelector("Audio").src=navigator   ;
+
